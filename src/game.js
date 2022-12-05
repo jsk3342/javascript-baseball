ae50adb195571a09c0dcfd8c0886ee564b6f7e11
jisu-kim <kss2460@naver.com>
2022-12-05 21:14:31 +0900

    chore: init

---
+++ src/game.js
@@ -0,0 +1,10 @@
+
+
+class BaseBallGame {
+    constructor(
+        
+    )
+    init() {
+
+    }
+}
---
+++ src/input.js
@@ -0,0 +1,5 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
+function startGame() {
+    MissionUtils.Console.readline('')
+}
