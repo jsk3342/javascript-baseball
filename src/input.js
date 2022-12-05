1c376854f89a5ad6ee5da321e7cbb60f64658891
jisu-kim <kss2460@naver.com>
2022-12-05 21:23:09 +0900

    feat(input): 입력 받기 구현

--- src/input.js
+++ src/input.js
@@ -1,5 +1,9 @@
-const { MissionUtils } = require("@woowacourse/mission-utils");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 function startGame() {
-    MissionUtils.Console.readline('')
+    MissionUtils.Console.readLine('숫자를 입력해주세요', (input) => {
+        console.log(input)
+    } )
 }
+
+module.exports = startGame;
