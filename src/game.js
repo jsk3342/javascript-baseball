da251e351f48d341ecb7f903eeca73f2f30afbff
jisu-kim <kss2460@naver.com>
2022-12-06 02:08:09 +0900

    feat(init): 초기 번호 세팅

--- src/game.js
+++ src/game.js
@@ -1,10 +1,21 @@
-
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class BaseBallGame {
-    constructor(
-        
-    )
-    init() {
-
+    constructor() {
+        this.computer = [];
     }
+
+    init() {
+        const computer = [];
+        while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+            computer.push(number);
+            }
+        }
+        this.computer = computer;
+    }
+
 }
+
+module.exports = BaseBallGame;
