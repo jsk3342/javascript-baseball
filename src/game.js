2226d93e1ffa29dd3f558044602504e16418a3e4
jisu-kim <kss2460@naver.com>
2022-12-06 21:30:15 +0900

    feat(BaseBallGame): 내부 로직 구현

--- src/game.js
+++ src/game.js
@@ -16,6 +16,32 @@ class BaseBallGame {
         this.computer = computer;
     }
 
+    compareNumber(input) {
+        const inputNumber = input.toString().split("").map(i => +i)
+        let strike = this.isStrike(inputNumber);
+        let ball = this.isBall(inputNumber)
+        return [strike, ball]
+    }
+
+    isStrike(inputNumber) {
+        let strike = 0;
+        for(let i = 0; i < inputNumber.length; i++){
+            if(inputNumber[i] === this.computer[i]) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    isBall(inputNumber) {
+        let ball = 0;
+        this.computer.forEach((number, index) => {
+            if(inputNumber.indexOf(number) !== -1 && inputNumber[index] !== number) {
+                ball++;
+            }
+        })
+        return ball;
+    }
 }
 
 module.exports = BaseBallGame;
